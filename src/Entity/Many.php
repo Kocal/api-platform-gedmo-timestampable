cc923fba4128ad4d5132659369d082b65cfc48ca
Hugo Alliaume <kocal@live.fr>
2019-08-02 06:45:44 +0200

    create Many entity

---
+++ src/Entity/Many.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(normalizationContext={"groups"={"ticket"}},
+ *     denormalizationContext={"groups"={"ticket"}})
+ * @ORM\Entity(repositoryClass="App\Repository\ManyRepository")
+ */
+class Many
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"ticket"})
+     */
+    private $text;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Ticket", inversedBy="many")
+     */
+    private $ticket;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getTicket(): ?Ticket
+    {
+        return $this->ticket;
+    }
+
+    public function setTicket(?Ticket $ticket): self
+    {
+        $this->ticket = $ticket;
+
+        return $this;
+    }
+}
--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -3,13 +3,20 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"={"ticket"}},
+ *     denormalizationContext={"groups"={"ticket"}}
+ * )
  */
 class Ticket
 {
@@ -25,8 +32,21 @@ class Ticket
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
+     * @Groups({"ticket"})
      */
     private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Many", mappedBy="ticket", cascade={"persist"})
+     * @ApiSubresource()
+     * @Groups({"ticket"})
+     */
+    private $many;
+
+    public function __construct()
+    {
+        $this->many = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -41,6 +61,37 @@ class Ticket
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Many[]
+     */
+    public function getMany(): Collection
+    {
+        return $this->many;
+    }
+
+    public function addMany(Many $many): self
+    {
+        if (!$this->many->contains($many)) {
+            $this->many[] = $many;
+            $many->setTicket($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMany(Many $many): self
+    {
+        if ($this->many->contains($many)) {
+            $this->many->removeElement($many);
+            // set the owning side to null (unless already changed)
+            if ($many->getTicket() === $this) {
+                $many->setTicket(null);
+            }
+        }
 
         return $this;
     }
